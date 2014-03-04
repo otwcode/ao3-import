60053400bf5a438028a654e1f430426a74205df6
elz <elzj78@gmail.com>
2014-03-03 20:14:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ao3Import::Application.config.secret_key_base = 'c1c02bf7ea2c415f126020e1cfc03221bc48870a258a3444f9bf09648d92f5cd9e64018ba967d816e37cc8554a3b001ecc2e40f9524d3e530ae35ad99881a1f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ao3Import::Application.config.session_store :cookie_store, key: '_ao3-import_session'
